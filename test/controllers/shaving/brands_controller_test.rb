a9dd671be425867a8452a2cd0f8f514ad921dc5b
Josh Ashby <jashby@tangogroup.com>
2014-08-04 16:21:03 -0600

    tests for controllers

--- test/controllers/shaving/brands_controller_test.rb
+++ test/controllers/shaving/brands_controller_test.rb
@@ -1,7 +1,68 @@
 require 'test_helper'
 
 class Shaving::BrandsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "does index return correctly?" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns :brands
+  end
+
+  test "does new return correctly?" do
+    get :new
+    assert_response :success
+    assert_not_nil assigns :brand
+  end
+
+  test "can we post an empty brand and have it render the new form?" do
+    post :create, shaving_brand: {name: ''}
+
+    assert_template :new
+  end
+
+  test "can we post a new brand and have it redirect corrrectly?" do
+    assert_difference('Shaving::Brand.all.count') do
+      post :create, shaving_brand: {name: 'Test'}
+    end
+
+    assert_redirected_to shaving_brand_path(assigns(:brand))
+  end
+
+  test "can we view the show page for a brand?" do
+    brand = create :shaving_brand
+
+    get :show, {:id => brand.id}
+
+    assert_template :show
+    assert_not_nil assigns :brand
+  end
+
+  test "can we view the edit page for a brand?" do
+    brand = create :shaving_brand
+
+    get :edit, {:id => brand.id}
+
+    assert_template :edit
+    assert_template partial: '_form'
+    assert_not_nil assigns :brand
+  end
+
+  test "can we update a brand?" do
+    brand = build :shaving_brand
+    brand.save
+
+    brand.name = "TestTestTest"
+
+    put :update, id: brand.id, shaving_brand: {name: brand.name}
+
+    assert_redirected_to shaving_brand_path(assigns(:brand))
+  end
+
+  test "can we delete a brand?" do
+    brand = build :shaving_brand
+    brand.save
+
+    delete :destroy, id: brand.id
+
+    assert_redirected_to shaving_brands_path
+  end
 end
--- test/controllers/shaving_controller_test.rb
+++ test/controllers/shaving_controller_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
 class ShavingControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index with jumbotron for no brands" do
+    get :index
+    assert_response :success
+
+    assert_template :index
+    assert_template layout: "layouts/application"
+
+    assert_select 'div.jumbotron' do
+      assert_select 'a.btn'
+    end
+  end
+
+  test "should get index with the ul list with one li" do
+    assert create :shaving_brand
+
+    get :index
+    assert_response :success
+
+    assert_template :index
+    assert_template layout: "layouts/application"
+
+    assert_select 'ul#shaving-supplies' do
+      assert_select 'li#brands'
+    end
+  end
+
+  test "should get index with the ul list and two li" do
+    assert create :shaving_product, :with_brand, :with_type
+
+    get :index
+    assert_response :success
+
+    assert_template :index
+    assert_template layout: "layouts/application"
+
+    assert_select 'ul#shaving-supplies' do
+      assert_select 'li#brands'
+      assert_select 'li#products'
+    end
+  end
+
+  test "should get index with the ul list and all li" do
+    assert create :shaving_product, :with_brand, :with_type, :with_reviews
+
+    get :index
+    assert_response :success
+
+    assert_template :index
+    assert_template layout: "layouts/application"
+
+    assert_select 'ul#shaving-supplies' do
+      assert_select 'li#brands'
+      assert_select 'li#products'
+      assert_select 'li#reviews'
+    end
+  end
 end
