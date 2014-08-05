940e104a691544dc19ca53dfdd10da4c9bb9adb9
Josh Ashby <jashby@tangogroup.com>
2014-08-05 10:04:38 -0600

    seed db with initial types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+types = [
+  "Blade",
+  "Soap",
+  "Razor",
+  "Alum Block",
+  "Brush"
+]
+
+types.each do |name|
+  Shaving::Type.create :name => name
+end
